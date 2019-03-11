02e33aece16f44c2fd879555cd3dc3e1bf1e2e0a
El-Nico <40669920+El-Nico@users.noreply.github.com>
2019-03-11 10:17:11 +0000

    fixed dbUri variable, oh god, oh god, oh god

--- app_api/model/dbConfig.js
+++ app_api/model/dbConfig.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var dbUri = "mongodb://localhost/coolmine-footballers"
 //process.env.NODE_ENV= 'production';
 if (process.env.NODE_ENV === 'production') {
-    dbURI = 'mongodb://el-nico:123%40abc2@ds125125.mlab.com:25125/footballerscloud';
+    dbUri = 'mongodb://el-nico:123%40abc2@ds125125.mlab.com:25125/footballerscloud';
 }
 
 mongoose.connect(dbUri, { useNewUrlParser: true });
